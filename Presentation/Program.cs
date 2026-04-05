51d9aef915fde25df39f719300f8fce432dd71db
CarlXD123 <cnavarroc@autonoma.edu.pe>
2026-04-05 15:04:32 -0500

    configuracion del entorno UAT con docker y .env

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -14,9 +14,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration
     .SetBasePath(Directory.GetCurrentDirectory())
-    //.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-    //.AddJsonFile("appsettings.Dev.json", optional: false, reloadOnChange: true);
-    .AddJsonFile("appsettings.UAT.json", optional: false, reloadOnChange: true);
+    //.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddJsonFile("appsettings.UAT.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables();
 
 // =======================
 // DB CONTEXT
